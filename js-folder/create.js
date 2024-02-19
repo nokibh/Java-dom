ee6259b970c0ad893649a8d4b1a3932409156890
nokibh <nokibhowlader2003@gmail.com>
2024-02-19 18:54:54 +0600

    added new

--- js-folder/create.js
+++ js-folder/create.js
@@ -2,3 +2,29 @@ document.getElementById('buy-btn').addEventListener('click', function () {
   const busTicket = document.getElementById('seat-section');
   busTicket.scrollIntoView('seat-section');
 });
+let buttons = document.querySelectorAll('.green-btn');
+
+for (const button of buttons) {
+  button.addEventListener('click', function () {
+    button.style.backgroundColor = 'green';
+  });
+}
+// seat selection
+const allbtn = document.getElementsByClassName('green-btn');
+let count = 8;
+for (const btn of allbtn) {
+  btn.addEventListener('click', function (e) {
+    count = count - 1;
+    setInnerText('seat-count', count);
+  });
+}
+// ticket tk
+const tickets = document.getElementById('ticket');
+for (const ticket of tickets) {
+  cons;
+}
+
+// funtion call
+function setInnerText(id, value) {
+  document.getElementById(id).innerText = value;
+}
